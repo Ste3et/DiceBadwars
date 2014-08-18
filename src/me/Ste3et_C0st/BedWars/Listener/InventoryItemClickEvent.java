72039ba8f3911e06aed998562ca033d6e7113c94
Matthias <Matthias@Matthias-PC.speedport.ip>
2014-08-18 19:53:55 +0200

    Dicecraft Bedwars Plugin

--- src/me/Ste3et_C0st/BedWars/Listener/InventoryItemClickEvent.java
+++ src/me/Ste3et_C0st/BedWars/Listener/InventoryItemClickEvent.java
@@ -1,12 +1,15 @@
 package me.Ste3et_C0st.BedWars.Listener;
 
 import org.bukkit.entity.Player;
+
 import me.Ste3et_C0st.DiceBedWars.GUI.main;
+
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
 public class InventoryItemClickEvent {
 	
-	
+	@EventHandler
 	public void onInventoryClickEvent(InventoryClickEvent e){
 		Player p = (Player) e.getWhoClicked();
 		if((e.getInventory() == null) || (e.getCurrentItem() == null) || (e.getCurrentItem().hasItemMeta() == false) ||
