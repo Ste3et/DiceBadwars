c6a7a9318503adb82a9094a0ec7c4c9d3108d4f3
Matthias <Matthias@Matthias-PC.speedport.ip>
2014-08-18 19:52:50 +0200

    D

--- src/me/Ste3et_C0st/DiceBedWars/GUI/main.java
+++ src/me/Ste3et_C0st/DiceBedWars/GUI/main.java
@@ -1,13 +1,17 @@
 package me.Ste3et_C0st.DiceBedWars.GUI;
 
+import java.util.HashMap;
+
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import com.gmail.fedmanddev.VillagerTrade;
-import com.gmail.fedmanddev.VillagerTradeApi;
 
 public class main {
+	
+	public static HashMap<Player, Inventory> invList = new HashMap<Player, Inventory>();
 	
 	public void openGUI_1(Player p){
 	
